22134a3c39195aeae71f0a95f01636a723d621b5
Mohammad Pourbehzad <pspipm27@gmail.com>
2025-01-03 22:43:54 +0330

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import setuptools
+
+setuptools.setup(
+    name="provinces-and-cities",
+    version="1.0.1",
+    description="Package of countries provinces and cities.",
+    url="https://github.com/mohammad-prb/provinces-and-cities-pypi",
+    project_urls={
+        "Source Code": "https://github.com/mohammad-prb/provinces-and-cities-pypi",
+    },
+    author="Mohammad Pourbehzad",
+    author_email="pspipm27@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    packages=["provinces_and_cities"],
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+)
